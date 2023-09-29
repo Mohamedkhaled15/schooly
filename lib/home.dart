25eacc2aa05e7a965da03786c85ad398192049c8
mohamedkhaled <mohammedkhaled156212gmail.com>
2023-09-29 20:11:38 +0300

    test2

---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('ابو عمو واحد بس'),
+
+      ),
+      body: Column(
+        children: [
+          Text('ياعم ورد عليهم '),
+        ],
+      ),
+    );
+  }
+}
