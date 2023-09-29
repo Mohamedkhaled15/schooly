783080cec8ddef9f16e7200db7b209a3aeea7b98
mohamedkhaled <mohammedkhaled156212gmail.com>
2023-09-29 22:55:36 +0300

    update_colors_names

--- lib/components/constants/color.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const color1 = Color(0xff345FB4);
-const color2 = Color(0xff6789CA);
-const color3 = Color(0xff313131);
-const color4 = Color(0xff777777);
-const color5 = Color(0xffA5A5A5);
---
+++ lib/constants/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xff345FB4);
+const secondary = Color(0xff6789CA);
+const black = Color(0xff313131);
+const gray = Color(0xff777777);
+const whiteGray = Color(0xffA5A5A5);
