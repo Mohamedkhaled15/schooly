f4af8e4c0d165ba57ccc3fe053e344464c488dc4
mohamedkhaled <mohammedkhaled156212gmail.com>
2023-09-30 00:03:31 +0300

    add_images_update_color

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 const primary = Color(0xff345FB4);
 const secondary = Color(0xff6789CA);
 const black = Color(0xff313131);
+const white = Color(0xffffffff);
 const gray = Color(0xff777777);
 const whiteGray = Color(0xffA5A5A5);
+const calenderSU = Color(0xffD4E2FF);
+const calenderGreen = Color(0xff0BAC00);
+const calenderLightGreen = Color(0xffA9F2A4);
+const calenderRed = Color(0xffE92020);
+const calenderLightRed = Color(0xffFFB1B1);
+const quizSlider = Color(0xff46D9BF);
+
 
